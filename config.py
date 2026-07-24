8ebc996c379875797fc0c4ad80876d4136591b0f
Nadiya2121 <misshadiya21@gmail.com>
2026-07-24 10:16:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,26 @@
 import os
 from pymongo import MongoClient
 
-# MongoDB Connection
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://MovieLinkbd:MovieLinkbd@cluster0.cmx4zn5.mongodb.net/?appName=Cluster0")
 client = MongoClient(MONGO_URI)
-db = client['neon_shortener']
+db = client['neon_shortener_pro']
 
-# MongoDB Collections
 urls_col = db['urls']
 settings_col = db['settings']
 analytics_col = db['analytics']
 
-# Default Ad Settings Get Helper
 def get_settings():
     settings = settings_col.find_one({"_id": "global_settings"})
     if not settings:
         default_data = {
             "_id": "global_settings",
+            "phaseCount": 2,  # 1, 2, or 3 Phase Redirect
             "popunderCode": "",
             "directLinkUrl": "",
             "bannerTop": "",
             "bannerBottom": "",
             "nativeAd": "",
-            "timerSeconds": 10
+            "timerSeconds": 8
         }
         settings_col.insert_one(default_data)
         return default_data
